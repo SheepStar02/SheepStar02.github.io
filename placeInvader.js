95b0e6d6a05d4a772962f945dda4ad28061801fe
Eric Lin <eric1167g06@gmail.com>
2020-11-06 23:23:16 -0500

    Player movement

--- placeInvader.js
+++ placeInvader.js
@@ -2,21 +2,30 @@ let background = document.getElementById("background");
 
 let potatoes = [];
 
+console.log(screen.width);
+
 for (let i = 0; i < 24; i++){
     let invader = document.createElement("img");
     invader.src = "invader.png";
     invader.style = "width:40;height:40;position:absolute;left:" + (screen.width/4+(i%8)*45) + "px;top:" + (50+parseInt((i/8))*45) + "px";
     potatoes.push(invader);
     background.appendChild(invader);
-}
+} 
+
+let player = document.createElement("img");
+player.src = "potato.png";
+player.style = "width:70;height:50;position:absolute;left:" + (screen.width/2) + "px;top:" + (screen.height/2) + "px";
+background.appendChild(player);
+
+potatoes[4].style["visibility"] = "hidden";
 
 let frameCount = 0;
-let id = setInterval(frame, 200);
+let id = setInterval(frame, 10);
 let interval = screen.width/100;
+let moveleft = false;
+let moveright = false;
 
-function frame(){
-
-    frameCount ++;
+function updateInvaders(){
 
     if ((parseInt(potatoes[0].style["left"]) <= screen.width/4 && interval < 0) || (parseInt(potatoes[7].style["left"]) >= screen.width*3/4 && interval > 0)){
         interval *= -1;
@@ -25,7 +34,6 @@ function frame(){
     for (invader of potatoes){
         invader.style["left"] = parseInt(invader.style["left"])+interval;
     }
-
 
     for (invader of potatoes){
 
@@ -37,6 +45,58 @@ function frame(){
         }
 
         frameCount = 0;
-        
     }
+ 
 }
+
+function frame(){
+
+    frameCount ++;
+
+    if (frameCount % 20 == 0){
+        updateInvaders();
+    }
+
+    if (window.onkeypress){
+        console.log(event.keyCode + " hello");
+    }
+
+    movePlayer();
+    
+}
+
+function movePlayer(){
+
+    if (moveleft){
+        player.style["left"] = parseInt(player.style["left"]) + 1;
+    }
+
+    else if (moveright){
+        player.style["left"] = parseInt(player.style["left"]) - 1;
+    }
+
+}
+
+document.addEventListener('keydown', function(event) {
+
+    if (event.keyCode == 68){
+
+        moveleft = true;
+    }
+
+    else if (event.keyCode == 65){
+
+        moveright = true;
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+
+    if (event.keyCode == 68){
+        moveleft = false;
+    }
+
+    else if (event.keyCode == 65){
+        moveright = false;
+    }
+});
