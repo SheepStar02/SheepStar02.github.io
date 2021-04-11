e853708fb1a19d2ef6940f36b5eacb75c45f2a0a
wetmanesl <eric1167g06@gmail.com>
2021-04-11 17:41:13 -0400

    Fresh start

---
+++ index.js
@@ -0,0 +1,24 @@
+function enterOver() {
+    document.getElementById("main-shed-image").src = "/images/Shed-Image-Over.png";
+}
+
+function enterOut() {
+    document.getElementById("main-shed-image").src = "/images/Shed-Image.png";
+}
+
+function enterShed() {
+    let image = document.getElementById("shed-image-div"), room = document.getElementById("room-menu-div");
+    image.classList.add("hide");
+    image.style.zIndex = "-1";
+
+    setTimeout(() => {
+        room.style.zIndex = "0";
+        room.style.visibility = "visible";
+        room.classList.add("show");
+        console.log("here");
+    }, 200);
+}
+
+function playMinesweeper() {
+    window.open("minesweeper", "_self");
+}
