6c58d39c29b0d2ddbe21ee42600b3d70b428764a
wetmanesl <eric1167g06@gmail.com>
2021-06-16 13:01:56 -0400

    Quake testing v1

--- scripts/quake/quake.js
+++ scripts/quake/quake.js
@@ -5,4 +5,100 @@ firebase.database().ref("testing").set({
     tiddies:"tiddies"
 });
 
+let playDirection = -1;
+const quakeMap = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,1,0,1,0,0,0,0,0,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0],
+    [0,0,1,1,0,1,0,0,0,1,1,0,0,0,0,0,0,1,0,0],
+    [0,0,1,0,0,1,1,1,0,0,1,1,1,1,1,1,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+
+function adjustWindow() {
+    console.log("changing");
+    Object.assign(document.getElementById("d1-board").style,{
+        width:(window.innerHeight*0.95)+"px",
+        height:(window.innerHeight*0.95)+"px"});
+}
+
+function loadMenu() {
+    window.addEventListener('resize', adjustWindow);
+    adjustWindow();
+
+}
+
+function beginGame() {
+
+    document.getElementById("b1-play-button").classList.add("hide");
+
+    Object.assign(document.getElementById("d2-board").style, {
+        left:'-50%',
+        top:'-50%'
+    });
+
+    for (let row = 0; row < quakeMap.length; row++){
+        for (col = 0; col < quakeMap.length; col++){
+
+            let mapTile = document.createElement("div");
+
+            mapTile.id = "maptile-" + row + "-" + col;
+            mapTile.classList.add("maptile");
+            Object.assign(mapTile.style, {
+                left: col*5 + "%",
+                top: row*5 + "%",
+            });
+
+            if (quakeMap[row][col] === 0){
+                mapTile.style.backgroundImage = 'url("/assets/images/quake/grass-test-1.png")';
+
+            } else if (quakeMap[row][col] === 1){
+                mapTile.style.backgroundImage = 'url("/assets/images/quake/grass-test-2.png")';
+            }
+
+            document.getElementById("d2-board").append(mapTile);
+        }
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+
+    if (event.keyCode === 40 || event.keyCode === 83){
+        playDirection = 0;
+    } else if (event.keyCode === 38 || event.keyCode === 87) {
+        playDirection = 1;
+    } else if (event.keyCode === 37 || event.keyCode === 65) {
+        playDirection = 2;
+    } else if (event.keyCode === 39 || event.keyCode === 68) {
+        playDirection = 3;    
+    }
+
+});
+
+setInterval(function (){
+    if (playDirection === 0 && parseInt(document.getElementById("d2-board").style.top.split("%")[0]) >= -99){
+        document.getElementById("d2-board").style.top = parseFloat(document.getElementById("d2-board").style.top.split("%")[0]) - 0.5 + "%";
+    } else if (playDirection === 1 && parseInt(document.getElementById("d2-board").style.top.split("%")[0]) <= -1){
+        document.getElementById("d2-board").style.top = parseFloat(document.getElementById("d2-board").style.top.split("%")[0]) + 0.5 + "%";
+    } else if (playDirection === 2 && parseInt(document.getElementById("d2-board").style.left.split("%")[0]) <= -1){
+        document.getElementById("d2-board").style.left = parseFloat(document.getElementById("d2-board").style.left.split("%")[0]) + 0.5 + "%";
+    } else if (playDirection === 3 && parseInt(document.getElementById("d2-board").style.left.split("%")[0]) >= -99){
+        document.getElementById("d2-board").style.left = parseFloat(document.getElementById("d2-board").style.left.split("%")[0]) - 0.5 + "%";
+    }
+},20);
+
 console.log(firebase.database()); 
+loadMenu();
