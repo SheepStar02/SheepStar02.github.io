d758d1fc73655bec497a4cb4bb31a79b92c98877
wetmanesl <eric1167g06@gmail.com>
2022-03-23 19:26:44 -0400

    MONOPOLY V 0.01

--- scripts/play.js
+++ scripts/play.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = new WebSocket("ws://webserveresl.herokuapp.com");
 
 document.onreadystatechange = () => {
     if (document.readyState === "complete"){
