128a5d1512ac2dfa65d50cc4da4b0fb5bd6eff7a
sheepstar02 <wetmanroyale@gmail.com>
2022-05-05 11:15:53 -0400

    JUMBLETYPE V 0.02

--- scripts/jumbletype.js
+++ scripts/jumbletype.js
@@ -1,22 +1,36 @@
 const jumbleWordbank = ["the","of","to","and","a","in","is","it","you","that","he","was","for","on","are","with","as","I","his","they","be","at","one","have","this","from","or","had","by","not","word","but","what","some","we","can","out","other","were","all","there","when","up","use","your","how","said","an","each","she","which","do","their","time","if","will","way","about","many","then","them","write","would","like","so","these","her","long","make","thing","see","him","two","has","look","more","day","could","go","come","did","number","sound","no","most","people","my","over","know","water","than","call","first","who","may","down","side","been","now","find","any","new","work","part","take","get","place","made","live","where","after","back","little","only","round","man","year","came","show","every","good","me","give","our","under","name","very","through","just","form","sentence","great","think","say","help","low","line","differ","turn","cause","much","mean","before","move","right","boy","old","too","same","tell","does","set","three","want","air","well","also","play","small","end","put","home","read","hand","port","large","spell","add","even","land","here","must","big","high","such","follow","act","why","ask","men","change","went","light","kind","off","need","house","picture","try","us","again","animal","point","mother","world","near","build","self","earth","father","head","stand","own","page","should","country","found","answer","school","grow","study","still","learn","plant","cover","food","sun","four","between","state","keep","eye","never","last","let","thought","city","tree","cross","farm","hard","start","might","story","saw","far","sea","draw","left","late","run","don't","while","press","close","night","real","life","few","north","open","seem","together","next","white","children","begin","got","walk","example","ease","paper","group","always","music","those","both","mark","often","letter","until","mile","river","car","feet","care","second","book","carry","took","science","eat","room","friend","began","idea","fish","mountain","stop","once","base","hear","horse","cut","sure","watch","color","face","wood","main","enough","plain","girl","usual","young","ready","above","ever","red","list","though","feel","talk","bird","soon","body","dog","family","direct","pose","leave","song","measure","door","product","black","short","numeral","class","wind","question","happen","complete","ship","area","half","rock","order","fire","south","problem","piece","told","knew","pass","since","top","whole","king","space","heard","best","hour","better","true","during","hundred","five","remember","step","early","hold","west","ground","interest","reach","fast","verb","sing","listen","six","table","travel","less","morning","ten","simple","several","vowel","toward","war","lay","against","pattern","slow","center","love","person","money","serve","appear","road","map","rain","rule","govern","pull","cold","notice","voice","unit","power","town","fine","certain","fly","fall","lead","cry","dark","machine","note","wait","plan","figure","star","box","noun","field","rest","correct","able","pound","done","beauty","drive","stood","contain","front","teach","week","final","gave","green","oh","quick","develop","ocean","warm","free","minute","strong","special","mind","behind","clear","tail","produce","fact","street","inch","multiply","nothing","course","stay","wheel","full","force","blue","object","decide","surface","deep","moon","island","foot","system","busy","test","record","boat","common","gold","possible","plane","stead","dry","wonder","laugh","thousand","ago","ran","check","game","shape","equate","hot","miss","brought","heat","snow","tire","bring","yes","distant","fill","east","paint","language","among","grand","ball","yet","wave","drop","heart","am","present","heavy","dance","engine","position","arm","wide","sail","material","size","vary","settle","speak","weight","general","ice","matter","circle","pair","include","divide","syllable","felt","perhaps","pick","sudden","count","square","reason","length","represent","art","subject","region","energy","hunt","probable","bed","brother","egg","ride","cell","believe","fraction","forest","sit","race","window","store","summer","train","sleep","prove","lone","leg","exercise","wall","catch","mount","wish","sky","board","joy","winter","sat","written","wild","instrument","kept","glass","grass","cow","job","edge","sign","visit","past","soft","fun","bright","gas","weather","month","million","bear","finish","happy","hope","flower","clothe","strange","gone","jump","baby","eight","village","meet","root","buy","raise","solve","metal","whether","push","seven","paragraph","third","shall","held","hair","describe","cook","floor","either","result","burn","hill","safe","cat","century","consider","type","law","bit","coast","copy","phrase","silent","tall","sand","soil","roll","temperature","finger","industry","value","fight","lie","beat","excite","natural","view","sense","ear","else","quite","broke","case","middle","kill","son","lake","moment","scale","loud","spring","observe","child","straight","consonant","nation","dictionary","milk","speed","method","organ","pay","age","section","dress","cloud","surprise","quiet","stone","tiny","climb","cool","design","poor","lot","experiment","bottom","key","iron","single","stick","flat","twenty","skin","smile","crease","hole","trade","melody","trip","office","receive","row","mouth","exact","symbol","die","least","trouble","shout","except","wrote","seed","tone","join","suggest","clean","break","lady","yard","rise","bad","blow","oil","blood","touch","grew","cent","mix","team","wire","cost","lost","brown","wear","garden","equal","sent","choose","fell","fit","flow","fair","bank","collect","save","control","decimal","gentle","woman","captain","practice","separate","difficult","doctor","please","protect","noon","whose","locate","ring","character","insect","caught","period","indicate","radio","spoke","atom","human","history","effect","electric","expect","crop","modern","element","hit","student","corner","party","supply","bone","rail","imagine","provide","agree","thus","capital","won't","chair","danger","fruit","rich","thick","soldier","process","operate","guess","necessary","sharp","wing","create","neighbor","wash","bat","rather","crowd","corn","compare","poem","string","bell","depend","meat","rub","tube","famous","dollar","stream","fear","sight","thin","triangle","planet","hurry","chief","colony","clock","mine","tie","enter","major","fresh","search","send","yellow","gun","allow","print","dead","spot","desert","suit","current","lift","rose","continue","block","chart","hat","sell","success","company","subtract","event","particular","deal","swim","term","opposite","wife","shoe","shoulder","spread","arrange","camp","invent","cotton","born","determine","quart","nine","truck","noise","level","chance","gather","shop","stretch","throw","shine","property","column","molecule","select","wrong","gray","repeat","require","broad","prepare","salt","nose","plural","anger","claim","continent","oxygen","sugar","death","pretty","skill","women","season","solution","magnet","silver","thank","branch","match","suffix","especially","fig","afraid","huge","sister","steel","discuss","forward","similar","guide","experience","score","apple","bought","led","pitch","coat","mass","card","band","rope","slip","win","dream","evening","condition","feed","tool","total","basic","smell","valley","nor","double","seat","arrive","master","track","parent","shore","division","sheet","substance","favor","connect","post","spend","chord","fat","glad","original","share","station","dad","bread","charge","proper","bar","offer","segment","slave","duck","instant","market","degree","populate","chick","dear","enemy","reply","drink","occur","support","speech","nature","range","steam","motion","path","liquid","log","meant","quotient","teeth","shell","neck"];
+const socket = new WebSocket("wss://webserveresl.herokuapp.com");
 
-function App () {
+function App (props) {
 
     const [darkMode, setDarkMode] = React.useState((localStorage.getItem("dark") === "true") && true || false),
     [screen, setScreen] = React.useState("begin"),
     [copied, setCopied] = React.useState(false),
+    [challenge, setChallenge] = React.useState(false),
     [results, setResults] = React.useState({words:[], answers:[], matches:0});
+
+    if (challenge && localStorage.getItem("challenged") === "true" && JSON.stringify(results.words) !== JSON.stringify(props.words)) {
+        let match = 0;
+        for (let word of props.words){if (localStorage.getItem("answers").split(",").includes(word)) match ++}
+        setResults({words:props.words,answers:localStorage.getItem("answers").split(","), matches: match})
+    } else if (challenge && localStorage.getItem("challenged") === "true" && JSON.stringify(localStorage.getItem("words").split(",")) !== JSON.stringify(props.words)) {
+        localStorage.removeItem("challenged");localStorage.removeItem("answers");localStorage.setItem("words",props.words);
+    }
 
     return (
         <div className = {`main-container${(darkMode && " dark" || "")}`}>
             <div className = "content-wrap">
                 <div className = "head">
-                    <input id = "darkmode" type = "checkbox" style = {{display: "none"}} onClick = {() => {localStorage.setItem("dark", !darkMode);setDarkMode(!darkMode)}} checked = {darkMode}></input>
-                    <label for = "darkmode" className = "darkmode">
-                        <i></i>
-                        <i></i>
-                        <div className = "darkmode-ball"></div>
-                    </label>
+                    <div className = "navbar">
+                        <button className = {(!challenge && "selected" || "")} onClick = {() => {if(screen !== "play"){setChallenge(!challenge);setScreen("begin")}}}>Free Play</button>
+                        <button className = {(challenge && "selected" || "")} onClick = {() => {if(screen !== "play"){setChallenge(!challenge);if(localStorage.getItem("challenged") === "true" && screen !== "end" && JSON.stringify(localStorage.getItem("words").split(",")) === JSON.stringify(props.words)){setScreen("end")}}}}>Daily Challenge</button>
+                        <input id = "darkmode" type = "checkbox" style = {{display: "none"}} onClick = {() => {localStorage.setItem("dark", !darkMode);setDarkMode(!darkMode)}} checked = {darkMode}></input>
+                        <label for = "darkmode" className = "darkmode">
+                            <i></i>
+                            <i></i>
+                            <div className = "darkmode-ball"></div>
+                        </label>
+                    </div>
                     <div className = "title">
                         <h1>Jumble Type</h1>
                         <h2>Test how many words you can remember!</h2>
@@ -27,20 +41,28 @@ function App () {
                         <div className = {`jumble-begin jumble-content ${(screen === "begin" && "show" || "hide")}`}>
                             <div className = "rules">       
                                 <h3>How it works</h3>
-                                <p>You'll be shown 10 words</p>
-                                <p>Remember them as best you can</p>
-                                <p>Write them back down in any order</p>
+                                {challenge && 
+                                <React.Fragment>
+                                    <p>Same rules as free play</p>
+                                    <p>Everyone has the same words</p>
+                                    <p>You only get 1 attempt. Make it count</p>
+                                </React.Fragment> ||
+                                <React.Fragment>
+                                    <p>You'll be shown 10 words</p>
+                                    <p>Remember them as best you can</p>
+                                    <p>Write them back down in any order</p>
+                                </React.Fragment>}
                             </div>
-                            <button className = "begin" onClick = {() => {setScreen("play")}}>Get Started</button>
+                            <button className = "begin" onClick = {() => {setScreen((localStorage.getItem("challenged") === "true" && challenge) && "end" || "play")}}>Get Started</button>
                         </div>
-                        <Jumble screen = {screen} started = {screen === "play"} setScreen = {setScreen} setResults = {setResults}/>
+                        <Jumble screen = {screen} setScreen = {setScreen} setResults = {setResults} started = {screen === "play"} challenge = {challenge} challengeWords = {props.words}/>
                         <div className = {`jumble-end jumble-content ${(screen === "end" && "show" || "hide")}`}>
                             <h1>Score: {results.matches}/10</h1>
                             <div className = "score">{results.words.map(word => (
                                 <p className = "result-word">{word}<img src = {`/images/jumbletype/${(results.answers.includes(word) && "correct" || "incorrect")}.png`}></img></p>
                             ))}</div>
                             <div className = "end-menu"><button onClick = {() => {setScreen("begin")}}>Return<img src = "/images/jumbletype/return.png"></img></button><button onClick = {() => {
-                                let text = "Jumble Type Results: \n";
+                                let text = `Jumble Type ${(challenge && "Daily " || "")}Results: \n`;
                                 for (let word of results.words) {results.answers.includes(word) ? text += "✅ " : text += "❌ "}
                                 text += "\nTry and beat my score: https://sheepstar02.github.io/jumbletype";
                                 navigator.clipboard.writeText(text);
@@ -71,9 +93,13 @@ class Jumble extends React.Component {
     }
 
     generateWords () {
-        for (let i = 0; i < 10; i++) {
-            do {var rand = Math.floor(Math.random() * 1000)} while (this.state.words.includes(jumbleWordbank[rand]));
-            this.state.words.push(jumbleWordbank[rand].toUpperCase());
+        if (this.props.challenge) {
+            this.state.words = this.props.challengeWords;
+        } else {
+            for (let i = 0; i < 10; i++) {
+                do {var rand = Math.floor(Math.random() * 1000)} while (this.state.words.includes(jumbleWordbank[rand]));
+                this.state.words.push(jumbleWordbank[rand].toUpperCase());
+            }
         }
     }
 
@@ -111,8 +137,10 @@ class Jumble extends React.Component {
         let score = 0;
         for (let word of this.state.words) {if (this.state.answers.includes(word)) score++}
         this.props.setResults({answers:this.state.answers,words:this.state.words,matches:score})
+        if (this.props.challenge){localStorage.setItem("challenged", true);localStorage.setItem("answers",this.state.answers);localStorage.setItem("words",this.state.words)}
         this.props.setScreen("end");
-        this.setState({words: [],answers: [],instructionNum : 0,wordNum: 0,started: false,delay: 3000});
+        this.setState({words:[],answers:[],instructionNum:0,wordNum:0,started:false,delay:3000});
+
     }
 
     updateWord () {
@@ -124,7 +152,6 @@ class Jumble extends React.Component {
     render () {
 
         if (!this.state.showGiveUp) {setTimeout(() => {this.setState({showGiveUp:true})}, 10)}
-
         if (this.props.started && !this.state.started) {
             this.state.started = true;
             this.generateWords();
@@ -148,7 +175,15 @@ class Jumble extends React.Component {
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-)
+socket.onopen = function () {
+}
+
+socket.onmessage = function (event) {
+    let data = JSON.parse(event.data);
+    if (data.type === "DAILY_JUMBLE_TYPE"){
+        ReactDOM.render(
+            <App words = {data.words}/>,
+            document.getElementById("root")
+        )
+    }
+}
